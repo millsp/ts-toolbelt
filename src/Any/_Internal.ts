6cdf043d308b7b08669ce5c13d24d87ac84d78da
pirix-gh <pierreantoine.urvoy@gmail.com>
2019-06-21 16:16:10 +0200

    docs

--- src/Any/_Internal.ts
+++ src/Any/_Internal.ts
@@ -1,7 +1,7 @@
 /** Describes the match strategy when matching types
- * * extends: X extends Y
- * * equals : X equals  Y
- * * loose  : X extends Y || Y extends X
- * * default: extends (unless mentioned)
+ * * `extends`: X extends Y
+ * * `equals` : X equals  Y
+ * * `loose`  : X extends Y || Y extends X
+ * * `default`: extends (unless mentioned)
  */
 export type Match = 'extends' | 'equals' | 'loose' | 'default'
--- src/Tuple/_Internal.ts
+++ src/Tuple/_Internal.ts
@@ -1,7 +1,7 @@
 /** Describes the size limit of a **tuple**
- * * min => minimum size
- * * max => maximum size
- * * reg => regular size
+ * * `min`: minimum size
+ * * `max`: maximum size
+ * * `reg`: regular size
  * (Useful for **tuple**s with optionals)
  */
 export type Limit = 'min' | 'max' | 'reg'
