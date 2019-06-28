a432b2faac4c8e24c42a6c33e2c42bfe0b087d96
pirix-gh <pierreantoine.urvoy@gmail.com>
2019-06-28 20:12:45 +0200

    comments

--- src/Any/Extends.ts
+++ src/Any/Extends.ts
@@ -21,4 +21,4 @@ export type Extends<A1 extends any, A2 extends any> =
     : (A1 extends A2 ? true : false)
 
 // Comes from the fact that `never` is a fall-through type that we want to
-// narrow down to `false`. So it means that `Extends<never, never>` is false
+// narrow down to `false`. So it means that `Extends<never, any>` is false
