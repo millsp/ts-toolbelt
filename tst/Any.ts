dba519cfbfd78d6f5d9cffe41b9433c7f5c4477f
pirix-gh <pierreantoine.urvoy@gmail.com>
2019-06-30 00:04:22 +0200

    fix tests

--- tst/Any.ts
+++ tst/Any.ts
@@ -25,66 +25,66 @@ checks([
 // EQUALS
 
 checks([
-    check<A.Equals<[0, 1], any>,            boolean,    Test.Pass>(),
-    check<A.Equals<any, [0, 1]>,            boolean,    Test.Pass>(),
-    check<A.Equals<any, [0, 1]>,            boolean,    Test.Pass>(),
-    check<A.Equals<0, 0>,                   true,       Test.Pass>(),
-    check<A.Equals<0, 1>,                   false,      Test.Pass>(),
-    check<A.Equals<0, number>,              false,      Test.Pass>(),
-    check<A.Equals<any, string>,            boolean,    Test.Pass>(),
-    check<A.Equals<string, any>,            boolean,    Test.Pass>(),
-    check<A.Equals<{}, object>,             true,       Test.Pass>(),
-    check<A.Equals<{a: any}, object>,       false,      Test.Pass>(),
-    check<A.Equals<object, {a: any}>,       false,      Test.Pass>(),
-    check<A.Equals<any[], Array<any>>,      true,       Test.Pass>(),
-    check<A.Equals<'a' | 'b', 'b' | 'a'>,   true,       Test.Pass>(),
-    check<A.Equals<'a', 'a'>,               true,       Test.Pass>(),
-    check<A.Equals<true | false, boolean>,  true,       Test.Pass>(),
+    check<A.Equals<[0, 1], any>,            0 | 1,    Test.Pass>(),
+    check<A.Equals<any, [0, 1]>,            0 | 1,    Test.Pass>(),
+    check<A.Equals<any, [0, 1]>,            0 | 1,    Test.Pass>(),
+    check<A.Equals<0, 0>,                   1,       Test.Pass>(),
+    check<A.Equals<0, 1>,                   0,      Test.Pass>(),
+    check<A.Equals<0, number>,              0,      Test.Pass>(),
+    check<A.Equals<any, string>,            0 | 1,    Test.Pass>(),
+    check<A.Equals<string, any>,            0 | 1,    Test.Pass>(),
+    check<A.Equals<{}, object>,             1,       Test.Pass>(),
+    check<A.Equals<{a: any}, object>,       0,      Test.Pass>(),
+    check<A.Equals<object, {a: any}>,       0,      Test.Pass>(),
+    check<A.Equals<any[], Array<any>>,      1,       Test.Pass>(),
+    check<A.Equals<'a' | 'b', 'b' | 'a'>,   1,       Test.Pass>(),
+    check<A.Equals<'a', 'a'>,               1,       Test.Pass>(),
+    check<A.Equals<1 | 0, 0 | 1>,  1,       Test.Pass>(),
 ])
 
 // ---------------------------------------------------------------------------------------
 // EXTENDS
 
 checks([
-    check<A.Extends<any, any>,              true,       Test.Pass>(),
-    check<A.Extends<[0, 1], any>,           true,       Test.Pass>(),
-    check<A.Extends<any, [0, 1]>,           false,      Test.Fail>(),
-    check<A.Extends<any, [0, 1]>,           boolean,    Test.Pass>(),
-    check<A.Extends<0, 0>,                  true,       Test.Pass>(),
-    check<A.Extends<0, 1>,                  false,      Test.Pass>(),
-    check<A.Extends<0, number>,             true,       Test.Pass>(),
-    check<A.Extends<any, string>,           boolean,    Test.Pass>(),
-    check<A.Extends<string, any>,           true,       Test.Pass>(),
-    check<A.Extends<{}, object>,            true,       Test.Pass>(),
-    check<A.Extends<{a: any}, object>,      true,       Test.Pass>(),
-    check<A.Extends<object, {a: any}>,      false,      Test.Pass>(),
-    check<A.Extends<any[], Array<any>>,     true,       Test.Pass>(),
-    check<A.Extends<'a' | 'b', 'b' | 'a'>,  true,       Test.Pass>(),
-    check<A.Extends<'a', 'a'>,              true,       Test.Pass>(),
-    check<A.Extends<never, never>,          false,      Test.Pass>(),
+    check<A.Extends<any, any>,              1,       Test.Pass>(),
+    check<A.Extends<[0, 1], any>,           1,       Test.Pass>(),
+    check<A.Extends<any, [0, 1]>,           0,      Test.Fail>(),
+    check<A.Extends<any, [0, 1]>,           0 | 1,    Test.Pass>(),
+    check<A.Extends<0, 0>,                  1,       Test.Pass>(),
+    check<A.Extends<0, 1>,                  0,      Test.Pass>(),
+    check<A.Extends<0, number>,             1,       Test.Pass>(),
+    check<A.Extends<any, string>,           0 | 1,    Test.Pass>(),
+    check<A.Extends<string, any>,           1,       Test.Pass>(),
+    check<A.Extends<{}, object>,            1,       Test.Pass>(),
+    check<A.Extends<{a: any}, object>,      1,       Test.Pass>(),
+    check<A.Extends<object, {a: any}>,      0,      Test.Pass>(),
+    check<A.Extends<any[], Array<any>>,     1,       Test.Pass>(),
+    check<A.Extends<'a' | 'b', 'b' | 'a'>,  1,       Test.Pass>(),
+    check<A.Extends<'a', 'a'>,              1,       Test.Pass>(),
+    check<A.Extends<never, never>,          0,      Test.Pass>(),
 ])
 
 // ---------------------------------------------------------------------------------------
 // IS
 
 checks([
-    check<A.Is<'xxxx', string, 'extends'>,  true,   Test.Pass>(),
-    check<A.Is<string, 'xxxx', 'extends'>,  false,  Test.Pass>(),
+    check<A.Is<'xxxx', string, 'extends'>,  1,   Test.Pass>(),
+    check<A.Is<string, 'xxxx', 'extends'>,  0,  Test.Pass>(),
 
-    check<A.Is<'xxxx', string, 'equals'>,   false,  Test.Pass>(),
-    check<A.Is<string, 'xxxx', 'equals'>,   false,  Test.Pass>(),
+    check<A.Is<'xxxx', string, 'equals'>,   0,  Test.Pass>(),
+    check<A.Is<string, 'xxxx', 'equals'>,   0,  Test.Pass>(),
 
-    check<A.Is<'xxxx', string, 'loose'>,    true,   Test.Pass>(),
-    check<A.Is<string, 'xxxx', 'loose'>,    true,   Test.Pass>(),
+    check<A.Is<'xxxx', string, 'loose'>,    1,   Test.Pass>(),
+    check<A.Is<string, 'xxxx', 'loose'>,    1,   Test.Pass>(),
 
-    check<A.Is<string, string | number, 'extends'>, true,       Test.Pass>(),
-    check<A.Is<string | number, string, 'extends'>, boolean,    Test.Pass>(),
+    check<A.Is<string, string | number, 'extends'>, 1,       Test.Pass>(),
+    check<A.Is<string | number, string, 'extends'>, 0 | 1,    Test.Pass>(),
 
-    check<A.Is<string, string | number, 'equals'>,  false,  Test.Pass>(),
-    check<A.Is<string | number, string, 'equals'>,  false,  Test.Pass>(),
+    check<A.Is<string, string | number, 'equals'>,  0,  Test.Pass>(),
+    check<A.Is<string | number, string, 'equals'>,  0,  Test.Pass>(),
 
-    check<A.Is<string, string | number, 'loose'>,   true,   Test.Pass>(),
-    check<A.Is<string | number, string, 'loose'>,   true,   Test.Pass>(),
+    check<A.Is<string, string | number, 'loose'>,   1,   Test.Pass>(),
+    check<A.Is<string | number, string, 'loose'>,   1,   Test.Pass>(),
 ])
 
 // ---------------------------------------------------------------------------------------
