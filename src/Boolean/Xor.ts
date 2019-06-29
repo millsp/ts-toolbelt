71b002f9b74138a8a5d96bfa12697345c15c7a6e
pirix-gh <pierreantoine.urvoy@gmail.com>
2019-06-29 20:14:32 +0200

    default option

--- src/Boolean/Xor.ts
+++ src/Boolean/Xor.ts
@@ -14,7 +14,7 @@ import {Format, FormatMap} from './_Internal'
  * type test2 = B.Xor<boolean, true> // boolean
  * ```
  */
-export type Xor<B1 extends boolean, B2 extends boolean, fmt extends Format> =
+export type Xor<B1 extends boolean, B2 extends boolean, fmt extends Format = 'b'> =
     (Equals<B1, boolean> & Equals<B2, boolean>) extends true // If one of them is boolean
     ? FormatMap[fmt][0 | 1]                                  // boolean
     : Equals<B1, B2> extends true                            // If both of them are equal
