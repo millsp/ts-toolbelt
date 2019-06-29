e1680d1fa2656f541dccd449ee06077fbc4b146e
pirix-gh <pierreantoine.urvoy@gmail.com>
2019-06-30 00:03:34 +0200

    fix Test

--- src/Test.ts
+++ src/Test.ts
@@ -1,9 +1,10 @@
 import {Repeat} from './Tuple/Repeat'
 import {Equals} from './Any/Equals'
 import {Compute} from './Any/Compute'
+import {True, False, Boolean} from './Boolean/_Boolean'
 
-export type Pass = true
-export type Fail = false
+export type Pass = True
+export type Fail = False
 
-export declare function check<Type, Expect, Outcome extends boolean>(debug?: Compute<Type>): Equals<Equals<Type, Expect, 'strict'>, Outcome>
-export declare function checks(asserts: Partial<Repeat<true, '30'>>): void;
+export declare function check<Type, Expect, Outcome extends Boolean>(debug?: Compute<Type>): Equals<Equals<Type, Expect, 'strict'>, Outcome>
+export declare function checks(asserts: Partial<Repeat<Pass, '30'>>): void;
