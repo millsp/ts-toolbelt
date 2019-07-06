97def4f09c11df0469d4b413f194da8770cb5ef3
pirix-gh <pierreantoine.urvoy@gmail.com>
2019-07-06 13:43:12 +0200

    simpler Pop

--- src/Tuple/Pop.ts
+++ src/Tuple/Pop.ts
@@ -6,6 +6,4 @@ import {Prev} from '../Iteration/Prev'
 import {Cast} from '../Any/Cast'
 
 export type Pop<T extends any[]> =
-    Omit<T, Key<Prev<IterationOf<Length<T, 's'>>>>> extends infer X
-    ? Cast<X, any[]>
-    : never
+    Omit<T, Key<Prev<IterationOf<Length<T, 's'>>>>>
