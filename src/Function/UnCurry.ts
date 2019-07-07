737b17688a3f67f39ecbdb2a42c3bee671b40754
pirix-gh <pierreantoine.urvoy@gmail.com>
2019-07-07 20:29:44 +0200

    Uncurry does not work, is on stand-by

--- src/Function/UnCurry.ts
+++ src/Function/UnCurry.ts
@@ -1,12 +1,11 @@
-import {Curry} from './Curry'
+// import {Curry} from './Curry'
 
-// Does not work, used to
-/** Undoes the work that was done by **`Curry`**
- * @param F to uncurry
- * @returns **`Function`**
- * @example
- */
-export type UnCurry<F extends Curry<any>> =
-    F extends Curry<infer UF>
-    ? UF
-    : never
+// /** Undoes the work that was done by **`Curry`**
+//  * @param F to uncurry
+//  * @returns **`Function`**
+//  * @example
+//  */
+// export type UnCurry<F extends Curry<any>> =
+//     F extends Curry<infer UF>
+//     ? UF
+//     : never
