c462e324734c5d01229bb22e0b85d5d723b6bc22
pirix-gh <pierreantoine.urvoy@gmail.com>
2019-07-25 15:16:49 +0200

    fix #22

--- src/String/Format.ts
+++ src/String/Format.ts
@@ -4,6 +4,17 @@ import {IterationOf} from '../Iteration/IterationOf'
 import {False, True} from '../Boolean/Boolean'
 import {Extends} from '../Any/Extends'
 
+/** Change the format of a **`string`**
+ * @param S to transform
+ * @returns **`string | number | boolean`**
+ * @example
+ * ```ts
+ * import {S} from 'ts-toolbelt'
+ *
+ * type test0 = S.Format<'30', 'b'> // True
+ * type test1 = S.Format<'30', 'n'> // 30
+ * ```
+ */
 export type Format<S extends string, fmt extends Formats> = {
     'b': {
         1: Boolean
