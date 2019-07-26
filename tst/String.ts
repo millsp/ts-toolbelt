e74c6628cfc48cd94762dee451038e031f05c0d8
pirix-gh <pierreantoine.urvoy@gmail.com>
2019-07-26 20:00:55 +0200

    added tests for String

--- tst/String.ts
+++ tst/String.ts
@@ -5,26 +5,13 @@ const {checks, check} = Test
 // ///////////////////////////////////////////////////////////////////////////////////////
 // STRING ////////////////////////////////////////////////////////////////////////////////
 
-// // ---------------------------------------------------------------------------------------
-// // BOOLEANOF
+// ---------------------------------------------------------------------------------------
+// FORMAT
 
-// checks([
-//     check<S.BooleanOf<'false'>,             0,      Test.Pass>(),
-//     check<S.BooleanOf<'true'>,              1,       Test.Pass>(),
-//     check<S.BooleanOf<'false' | 'true'>,    0 | 1,    Test.Pass>(),
-//     check<S.BooleanOf<string>,              0 | 1,    Test.Pass>(),
-//     check<S.BooleanOf<'xxxx'>,              1,       Test.Pass>(),
-//     check<S.BooleanOf<never>,               never,      Test.Pass>(),
-// ])
-
-// // ---------------------------------------------------------------------------------------
-// // NUMBEROF
-
-// checks([
-//     check<S.NumberOf<'420'>,        number,     Test.Pass>(),
-//     check<S.NumberOf<'-20'>,        -20,        Test.Pass>(),
-//     check<S.NumberOf<'12' | '-1'>,  12 | -1,    Test.Pass>(),
-//     check<S.NumberOf<string>,       number,     Test.Pass>(),
-//     check<S.NumberOf<'xxxx'>,       number,     Test.Pass>(),
-//     check<S.NumberOf<never>,        never,      Test.Pass>(),
-// ])
+checks([
+    check<S.Format<'xxxx', 'b'>,    1,          Test.Pass>(),
+    check<S.Format<'false', 'b'>,   0,          Test.Pass>(),
+    check<S.Format<'xxxx', 'n'>,    number,     Test.Pass>(),
+    check<S.Format<'10', 'n'>,      10,         Test.Pass>(),
+    check<S.Format<'100', 's'>,     '100',      Test.Pass>(),
+])
