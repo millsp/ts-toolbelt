af8999bdca8144721fa5c817a923be2362a8b6f8
pirix-gh <pierreantoine.urvoy@gmail.com>
2019-08-01 16:34:02 +0200

    fix(release): changed HEAD to $BRANCH

--- release.sh
+++ release.sh
@@ -10,7 +10,7 @@ git commit -m "chore(package.json): organize" &&
 BRANCH=`git rev-parse --symbolic-full-name --abbrev-ref HEAD` &&
 
 # # Generate the changelog then push to the current branch
-npx standard-version && git push --follow-tags origin HEAD &&
+npx standard-version && git push --follow-tags origin $BRANCH &&
 
 # # Publish the current branch as a tagged npm package
 if [ "$BRANCH" = "master" ]; then 
