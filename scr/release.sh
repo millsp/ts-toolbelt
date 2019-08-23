13708dfc04482721cfd8b5aafd689d24c1e8ccb6
pirix-gh <pierreantoine.urvoy@gmail.com>
2019-08-23 17:33:36 +0200

    fix: don't push tags on non-master branch

--- scr/release.sh
+++ scr/release.sh
@@ -11,11 +11,13 @@ npx sort-package-json &&
 BRANCH=`git rev-parse --symbolic-full-name --abbrev-ref HEAD` &&
 
 # # Generate the changelog then push to the current branch
-npx standard-version && git push --follow-tags origin $BRANCH &&
+npx standard-version &&
 
 # # Publish the current branch as a tagged npm package
-if [ "$BRANCH" = "master" ]; then 
+if [ "$BRANCH" = "master" ]; then
+    git push --follow-tags origin $BRANCH &&
     npm publish
 else
+    git push origin $BRANCH &&
     npm publish --tag $BRANCH  
 fi;
