054b2a3a8668aff07f6ee4cb835ba97b20ac4423
pirix-gh <pierreantoine.urvoy@gmail.com>
2019-08-23 22:35:34 +0200

    ci: tests for dt

--- scr/release:ci.sh
+++ scr/release:ci.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
+rm -fr dt
+
+npm run test &&
+
+git clone --depth=1 https://github.com/DefinitelyTyped/DefinitelyTyped.git dt &&
+
+npx ts-node release:ci/dt-update-version.ts &&
+
+cd dt &&
+
+git commit -am "prepared tests" &&
+
+npm i &&
+
 npm run test
+
+rm -fr dt
---
+++ scr/release:ci/dt-update-version.ts
@@ -0,0 +1,34 @@
+import * as fs from 'fs'
+
+// update the ts-toolbelt dependents to the latest ts-toolbelt version
+const dtUpdateVersion = (path: string) => fs.readdir(path, 'utf8', (error, docs) => {
+    if (error)
+        console.error(error)
+    else {
+        docs.forEach((doc) => {
+            doc = `${path}/${doc}`
+
+            if (fs.statSync(doc).isDirectory())
+                dtUpdateVersion(`${doc}`)
+            else {
+                fs.readFile(doc, 'utf8', (error, data) => {
+                    if (error) throw error
+
+                    const match = /"ts-toolbelt": ".*"/u
+
+                    if (doc.match(/package.json/u) && data.match(match)) {
+                        data = data.replace(match, '"ts-toolbelt": "next"')
+
+                        fs.writeFile(doc, data, (error) => {
+                            if (error) throw error
+
+                            console.info(`updated ${doc}`)
+                        })
+                    }
+                })
+            }
+        })
+    }
+})
+
+dtUpdateVersion('./dt')
