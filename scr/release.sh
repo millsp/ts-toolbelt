748e8e71f0f1081baba3a77b290e243c24c41f2d
pirix-gh <pierreantoine.urvoy@gmail.com>
2019-08-27 22:24:10 +0200

    ci: debug

--- scr/release.sh
+++ scr/release.sh
@@ -12,11 +12,11 @@ BRANCH=`git rev-parse --symbolic-full-name --abbrev-ref HEAD` &&
 
 # Publish the current branch
 if [ "$BRANCH" = "master" ]; then
-    npm run build:docs &&
+    #npm run build:docs &&
 
-    npx standard-version &&
+    #npx standard-version &&
 
-    git push --follow-tags origin $BRANCH
+    git push #--follow-tags origin $BRANCH
 else
     git push origin $BRANCH
 fi;
