33204c6339abbc6963511a11c53a3f17b99f4e2f
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-09-10 15:44:25 +0200

    test: travis works

--- src/Function/Curry.ts
+++ src/Function/Curry.ts
@@ -54,7 +54,7 @@ export type Curry<F extends Function> =
     <T extends any[]>(...args: Cast<T, Gaps<Parameters<F>>>) =>
         GapsOf<T, Parameters<F>> extends infer G
         ? Length<Cast<G, any[]>> extends infer L
-          ? L extends 1
+          ? L extends 0 ? Return<F> : L extends 1
             ? (...args: Cast<G, any[]>)       => Return<F>
             : Curry<(...args: Cast<G, any[]>) => Return<F>>
         : never
