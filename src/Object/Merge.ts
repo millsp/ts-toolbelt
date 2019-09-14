06e26712c34254c40b256122013983ab394f4d04
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-09-14 13:37:31 +0200

    refactor: simplify Merge

--- src/Object/Merge.ts
+++ src/Object/Merge.ts
@@ -1,12 +1,8 @@
 import {Omit} from './Omit'
+import {Compute} from '../Any/Compute'
 
-type _Merge<O extends object, O1 extends object> = {
-    [K in keyof (O & O1)]: K extends keyof O1
-                         ? O1[K]
-                         : K extends keyof O
-                           ? O[K]
-                           : never
-}
+type _Merge<O extends object, O1 extends object> =
+  Compute<O & Omit<O1, keyof O>>
 
 /** Complete the fields of **`O`** with the ones of **`O1`**
  * @param O to complete
@@ -17,6 +13,6 @@ type _Merge<O extends object, O1 extends object> = {
  * ```
  */
 export type Merge<O extends object, O1 extends object> =
-    _Merge<O, Omit<O1, keyof O>>
+    _Merge<O, O1>
     // We make sure keys do NOT overlap
     // Otherwise we'd loose field meta
