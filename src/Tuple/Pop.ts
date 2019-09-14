17bb61fe7c42285b9a287e2d32b8f20d216de404
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-09-14 21:44:53 +0200

    fix: allow Pop to work with optionals

--- src/Tuple/Pop.ts
+++ src/Tuple/Pop.ts
@@ -3,6 +3,7 @@ import {Key} from '../Iteration/Key'
 import {Length} from './Length'
 import {IterationOf} from '../Iteration/IterationOf'
 import {Prev} from '../Iteration/Prev'
+import {Max} from '../Number/Max'
 
 export type Pop<T extends any[]> =
-    Omit<T, Key<Prev<IterationOf<Length<T, 's'>>>>>
+    Omit<T, Key<Prev<IterationOf<Max<Length<T, 's'>>>>>>
