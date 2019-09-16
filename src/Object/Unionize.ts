308e78b3180778350df26bbc39a82c1ff7f4927a
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-09-16 17:18:58 +0200

    feat: added possibility to specify keys

--- src/Object/Unionize.ts
+++ src/Object/Unionize.ts
@@ -1,13 +1,15 @@
 import {At} from './At'
+import {Index} from '../_Internal'
 
 /** Make the fields of **`O`** union the ones of **`O1`**
  * @param O to union from
  * @param O1 to union with
+ * @param K to chose fields (?=`keyof O`)
  * @returns **`object`**
  * @example
  * ```ts
  * ```
  */
-export type Unionize<O extends object, O1 extends object> = {
-    [K in keyof O]: O[K] | At<O1, K>
+export type Unionize<O extends object, O1 extends object, K extends Index = keyof O> = {
+    [P in keyof O]: P extends K ? O[P] | At<O1, P> : never
 }
