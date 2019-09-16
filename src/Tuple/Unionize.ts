79c2332563b716c3b6f7ad1d77947c6f23acf729
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-09-16 19:15:03 +0200

    fix: useless fields are not to be picked

--- src/Tuple/Unionize.ts
+++ src/Tuple/Unionize.ts
@@ -1,5 +1,6 @@
-import {Unionize} from '../Object/Unionize'
 import {Index} from '../_Internal'
+import {At} from './At'
+import {Pick} from './Pick'
 
 /** Make the fields of **`T`** union the ones of **`T1`**
  * @param T to union from
@@ -10,5 +11,6 @@ import {Index} from '../_Internal'
  * ```ts
  * ```
  */
-export type Unionize<T extends object, T1 extends object, K extends Index = keyof T> =
-    Unionize<T, T1, K>
+export type Unionize<T extends any[], T1 extends any[], K extends Index = keyof T> = Pick<{
+    [P in keyof T]: P extends K ? T[P] | At<T1, P> : never
+}, K> // Pick makes sure we don't have excess `never`
