9e460d19198d5f765b66d4fcba180c8789f3fb93
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-09-16 17:21:03 +0200

    fix: added K feature

--- src/Tuple/Unionize.ts
+++ src/Tuple/Unionize.ts
@@ -1,12 +1,14 @@
 import {Unionize} from '../Object/Unionize'
+import {Index} from '../_Internal'
 
 /** Make the fields of **`T`** union the ones of **`T1`**
  * @param T to union from
  * @param T1 to union with
+ * @param depth to do it deeply (?=`'flat'`)
  * @returns **`any[]`**
  * @example
  * ```ts
  * ```
  */
-export type Unionize<T extends object, T1 extends object> =
-    Unionize<T, T1>
+export type Unionize<T extends object, T1 extends object, K extends Index = keyof T> =
+    Unionize<T, T1, K>
