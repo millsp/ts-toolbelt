833c35200cdb937daec9d07cecbff478b43cae16
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-09-17 11:07:38 +0200

    fix: shorter-simpler Merge and Merge 'deep'

--- src/Object/Merge.ts
+++ src/Object/Merge.ts
@@ -1,47 +1,29 @@
 import {Omit} from './Omit'
-import {Extends} from '../Any/Extends'
 import {At} from './At'
-import {And} from '../Boolean/And'
-import {True} from '../Boolean/Boolean'
 import {Compute} from '../Any/Compute'
-import {Index} from '../_Internal'
 import {Depth} from './_Internal'
-
-type MergeProp<O extends object, O1P extends object, K extends Index> =
-    K extends keyof O
-    ? O[K]
-    : K extends keyof O1P
-      ? O1P[K]
-      : never
+import {Kind} from '../Any/Kind'
 
 type MergeFlat<O extends object, O1 extends object> =
-    _MergeFlat<O, Omit<O1, keyof O>>
-    // We need to exclude `O` out of `O1` to preserve modifiers
-    // because doing `keyof (O & O1P)` mixes the modifiers up
+    Compute<O & Omit<O1, keyof O>>
 
-type _MergeFlat<O extends object, O1P extends object> = {
-    [K in keyof (O & O1P)]: MergeProp<O, O1P, K>
-    // for each property, pick what's available in `O` or `O1`
-    // at this stage `O1P` is just a part of the original `O1`
-    // => it's the part of `O1` that does not overlap with `O`
-}
+type MergeDeep<O, O1> =
+    Kind<(O | O1)> extends 'object'
+    ? MergeFlat<O & {}, O1 & {}> extends infer M
+      ? {[K in keyof M]: MergeDeep<M[K], At<O1 & {}, K>>}
+      : never
+    : O
 
-type MergeDeep<O extends object, O1 extends object> =
-    Compute<_MergeDeep<O, Omit<O1, keyof O>, O1>>
-    // same principle as above, but with a little tweak
-    // we keep the original `O1` to know if we can merge it
-    // => if `O` and `O1` have `object` fields of same name
-    // => then it means that both fields can be merged deeply
-    // => and this is the meaning of what is programmed below
-
-type _MergeDeep<O extends object, O1P extends object, O1 extends object> = {
-    [K in keyof (O & O1P)]: And<Extends<At<O, K>, object>, Extends<At<O1, K>, object>> extends True
-                            ? MergeDeep<At<O, K> & {}, At<O1, K> & {}>
-                            : MergeProp<O, O1, K>
-}
+// If we wanted to dive in the tuples as well
+// Kind<(O | O1)> extends 'array'
+// ? TMerge<Cast<O, any[]>, Cast<O1, any[]>> extends infer M
+//   ? {[K in keyof M]: MergeDeep<M[K], At<O1 & {}, K>>}
+//   : never // this is a trick to force ts to do it deeply
+// : O       // in versions <= 3.7 no-recursive-conditional
 
 /** Complete the fields of **`O`** with the ones of **`O1`**
  * ('deep' option will skip any nullable object to be merged)
+ * (only pure)
  * @param O to complete
  * @param O1 to copy from
  * @param depth to do it deeply (?=`'flat'`)
--- src/Tuple/Merge.ts
+++ src/Tuple/Merge.ts
@@ -1,38 +1,25 @@
 import {Omit} from '../Object/Omit'
-import {Extends} from '../Any/Extends'
+import {Merge as OMerge} from '../Object/Merge'
 import {At} from '../Object/At'
-import {And} from '../Boolean/And'
-import {True} from '../Boolean/Boolean'
-import {Compute} from '../Any/Compute'
-import {Index} from '../_Internal'
 import {Depth} from '../Object/_Internal'
 import {ObjectOf} from './ObjectOf'
 import {TupleOf} from '../Object/TupleOf'
-
-// todo One day, make this cleaner with type references
-
-type MergeProp<T extends object, T1P extends object, K extends Index> =
-    K extends keyof T
-    ? T[K]
-    : K extends keyof T1P
-      ? T1P[K]
-      : never
+import {Length} from './Length'
+import {Kind} from '../Any/Kind'
 
 type MergeFlat<T extends any[], T1 extends any[]> =
-    TupleOf<_MergeFlat<ObjectOf<T>, Omit<ObjectOf<T1>, keyof T>>>
+    number extends Length<T | T1>
+    // if we can't know the size, then get closest type
+    ? (T | T1) extends (infer T)[] ? T[] : never
+    // if it's a tuple then we proceed with the merging
+    : TupleOf<OMerge<ObjectOf<T>, Omit<ObjectOf<T1>, keyof T>>>
 
-type _MergeFlat<T extends object, T1P extends object> = {
-    [K in keyof (T & T1P)]: MergeProp<T, T1P, K>
-}
-
-type MergeDeep<T extends any[], T1 extends any[]> =
-    TupleOf<Compute<_MergeDeep<ObjectOf<T>, Omit<ObjectOf<T1>, keyof T>, ObjectOf<T1>>>>
-
-type _MergeDeep<T extends object, T1P extends object, T1 extends object> = {
-    [K in keyof (T & T1P)]: And<Extends<At<T, K>, any[]>, Extends<At<T1, K>, any[]>> extends True
-                            ? MergeDeep<At<T, K> & [], At<T1, K> & []>
-                            : MergeProp<T, T1, K>
-}
+type MergeDeep<O, O1> =
+    Kind<(O | O1)> extends 'array'
+    ? MergeFlat<O & [], O1 & []> extends infer M
+      ? {[K in keyof M]: MergeDeep<M[K], At<O1 & [], K>>}
+      : never
+    : O
 
 /** Complete the fields of **`O`** with the ones of **`O1`**
  * ('deep' option will skip any nullable object to be merged)
