c6a7190f60a18f90c986c4178d42c3d15fae03f0
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-09-17 11:13:05 +0200

    docs: comments

--- src/Any/Kind.ts
+++ src/Any/Kind.ts
@@ -8,9 +8,9 @@
 export type Kind<A extends any> =
     A extends string   ? 'string'  :
     A extends number   ? 'number'  :
-    A extends Function ? 'function': // the order
-    A extends any[]    ? 'array'   : // of this is
-    A extends object   ? 'object'  : // important
+    A extends Function ? 'function': // the order  // to
+    A extends any[]    ? 'array'   : // of this is // untangle
+    A extends object   ? 'object'  : // important  // object types
     A extends number   ? 'number'  :
     A extends boolean  ? 'boolean' :
     'unknown'
