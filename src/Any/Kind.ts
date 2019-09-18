705847f624a3184e0ad191dc01f28b7eac45fd4f
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-09-18 21:19:22 +0200

    fix: remove extra number check

--- src/Any/Kind.ts
+++ src/Any/Kind.ts
@@ -3,7 +3,7 @@ import {True} from '../Boolean/_api'
 
 /** Get the literal kind of a type
  * @param A
- * @returns **`'string' | 'number' | 'function' | 'array' | 'object' | 'number' | 'boolean'`**
+ * @returns **`'string' | 'number' | 'function' | 'array' | 'object' | 'boolean'`**
  * @example
  * ```ts
  * ```
@@ -11,9 +11,8 @@ import {True} from '../Boolean/_api'
 export type Kind<A extends any> =
     Extends<A, string> extends True   ? 'string'  :
     Extends<A, number> extends True   ? 'number'  :
+    Extends<A, boolean> extends True  ? 'boolean' :
     Extends<A, Function> extends True ? 'function': // the order  // to
     Extends<A, any[]> extends True    ? 'array'   : // of this is // untangle
     Extends<A, object> extends True   ? 'object'  : // important  // object types
-    Extends<A, number> extends True   ? 'number'  :
-    Extends<A, boolean> extends True  ? 'boolean' :
     'unknown'
