d17ff1b7e017af26f532b0787bcf88f1ba0f0187
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-09-18 21:08:08 +0200

    chore: cleanup

--- src/Object/CompulsoryKeys.ts
+++ src/Object/CompulsoryKeys.ts
@@ -1,4 +1,5 @@
 import {FilterKeys} from './FilterKeys'
+import {Index} from '../_Internal'
 
 /** Get the keys of **`O`** that are compulsory
  * @param O
@@ -8,5 +9,6 @@ import {FilterKeys} from './FilterKeys'
  * ```
  */
 export type CompulsoryKeys<O extends object> =
-    FilterKeys<O, undefined, '<-extends'> & FilterKeys<O, null, '<-extends'>
-
+    FilterKeys<O, undefined, '<-extends'> &
+    FilterKeys<O, null, '<-extends'> &
+    Index
