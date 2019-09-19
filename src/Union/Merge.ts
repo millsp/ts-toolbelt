7e636ad281ab43b7abbbb9feceab268f467ad337
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-09-19 13:41:29 +0200

    feat: added Merge for object unions

---
+++ src/Union/Merge.ts
@@ -0,0 +1,20 @@
+import {At} from '../Object/At'
+import {Overwrite} from '../Object/Overwrite'
+import {Compute} from '../Any/Compute'
+import {IntersectOf} from './IntersectOf'
+import {NullableKeys} from '../Object/NullableKeys'
+import {Pick} from '../Object/Pick'
+
+/** Merge a **union** of **`object`**s into a single one
+ * @param U to merge
+ * @returns **`object`**
+ * @example
+ * ```ts
+ * ```
+ */
+export type Merge<U extends object> = Compute<IntersectOf<Overwrite<U, {
+    [K in keyof U]-?: U extends unknown
+                      ? At<U, K>
+                      : never
+}>>>
+
