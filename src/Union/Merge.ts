e08a32b2b192aa23c21ace552c7b455fff0aca9b
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-09-19 16:47:35 +0200

    feat: added Union/Merge

--- src/Union/Merge.ts
+++ src/Union/Merge.ts
@@ -2,6 +2,13 @@ import {At} from '../Object/At'
 import {Overwrite} from '../Object/Overwrite'
 import {Compute} from '../Any/Compute'
 import {IntersectOf} from './IntersectOf'
+import {Strict} from './Strict'
+
+export type _Merge<U extends object> =IntersectOf<Overwrite<U, {
+    [K in keyof U]-?: U extends unknown
+                      ? At<U, K>
+                      : never
+}>>
 
 /** Merge a **union** of **`object`**s into a single one
  * @param U to merge
@@ -10,8 +17,5 @@ import {IntersectOf} from './IntersectOf'
  * ```ts
  * ```
  */
-export type Merge<U extends object> = Compute<IntersectOf<Overwrite<U, {
-    [K in keyof U]-?: U extends unknown
-                      ? At<U, K>
-                      : never
-}>>>
+export type Merge<U extends object> =
+    Compute<_Merge<Strict<U>>>
