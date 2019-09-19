c88c59b10b27c7f8bb3377b9e1b98ebccac33ec3
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-09-19 19:13:10 +0200

    ci: preserve doc history

--- scr/ci/docs/bd.sh
+++ scr/ci/docs/bd.sh
@@ -2,3 +2,8 @@
 
 npm run build:clean
 npm run build:docs
+
+# get the current version of the package
+DOCS_VERSION=$(node -p "require('./package.json').version")
+
+rsync -a "$PWD/docs/" "${PWD}/docs/${DOCS_VERSION}"
