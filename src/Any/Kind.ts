44ecbcab46040b03320666d99e200d0214234be4
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-09-20 18:08:11 +0200

    fix: change priority

--- src/Any/Kind.ts
+++ src/Any/Kind.ts
@@ -1,5 +1,6 @@
 import {Extends} from './Extends'
 import {True} from '../Boolean/Boolean'
+import {At} from '../Object/At'
 
 /** Get the literal kind of a type
  * @param A
@@ -9,10 +10,12 @@ import {True} from '../Boolean/Boolean'
  * ```
  */
 export type Kind<A extends any> =
-    Extends<A, string> extends True   ? 'string'  :
-    Extends<A, number> extends True   ? 'number'  :
-    Extends<A, boolean> extends True  ? 'boolean' :
     Extends<A, Function> extends True ? 'function': // the order  // to
     Extends<A, any[]> extends True    ? 'array'   : // of this is // untangle
     Extends<A, object> extends True   ? 'object'  : // important  // object types
+    Extends<A, string> extends True   ? 'string'  :
+    Extends<A, number> extends True   ? 'number'  :
+    Extends<A, boolean> extends True  ? 'boolean' :
     'unknown'
+
+type t = Kind<{} | (() => {})>
