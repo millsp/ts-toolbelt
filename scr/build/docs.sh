fc2267443fb0daf5cba348213516c3dbb6cd5708
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-09-24 14:14:54 +0200

    fix: crash typedoc

--- scr/build/docs.sh
+++ scr/build/docs.sh
@@ -6,6 +6,10 @@ mkdir -p docs
 # get the current version of the package
 DOCS_VERSION=$(node -p "require('./package.json').version")
 
+# force typedoc to use higher ts version
+rm -rf node_modules/typedoc/node_modules/typescript
+# !\ can cause bugs, be vigilent with it
+
 # generate docs their own version folder
 npx typedoc --out "docs/${DOCS_VERSION}" src --theme node_modules/eledoc/bin/default/ && 
 
