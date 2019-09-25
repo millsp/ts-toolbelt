ab2f1ca66e3a5b88f993f04b317aa0560a9067b4
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-09-25 12:18:14 +0200

    docs: for Either

--- src/Object/Either.ts
+++ src/Object/Either.ts
@@ -6,9 +6,9 @@ import {Boolean, True} from '../Boolean/Boolean'
 import {Compute} from '../Any/Compute'
 
 type _Either<O extends object, K extends Index> =
-    (Omit<O, K> & {          // Merge all but K
+    Omit<O, K> & {           // Merge all but K
         [P in K]: Pick<O, P> // With K possibilities
-    }[K])
+    }[K]
 
 type EitherStrict<O extends object, K extends Index> =
     Strict<_Either<O, K>> & {}
@@ -20,6 +20,7 @@ type EitherLoose<O extends object, K extends Index> =
  * the keys are ever present with one another within the different unions.
  * @param O to split
  * @param K to split with
+ * @param strict to force excess property checks (?=`True`) https://github.com/microsoft/TypeScript/issues/20863
  * @returns **`object`** **union**
  * @example
  * ```ts
--- src/Tuple/Either.ts
+++ src/Tuple/Either.ts
@@ -3,15 +3,17 @@ import {Either as OEither} from '../Object/Either'
 import {ObjectOf} from './ObjectOf'
 import {TupleOf} from '../Object/TupleOf'
 import {Tuple} from './Tuple'
+import {True, Boolean} from '../Boolean/Boolean'
 
 /** Split **`T`** into a **union** with **`K`** keys in such a way that none of
  * the keys are ever present with one another within the different unions.
  * @param T to split
  * @param K to split with
+ * @param strict to force excess property checks (?=`True`) https://github.com/microsoft/TypeScript/issues/20863
  * @returns **`any[]`** **union**
  * @example
  * ```ts
  * ```
  */
-export type Either<T extends Tuple, K extends Index> =
-    TupleOf<OEither<ObjectOf<T>, K>>
+export type Either<T extends Tuple, K extends Index, strict extends Boolean = True> =
+    TupleOf<OEither<ObjectOf<T>, K, strict>>
