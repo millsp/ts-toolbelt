24f7560cac9670fd9512f501c4c0fc24a1922f7b
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-09-25 15:48:43 +0200

    fix: version bump on branches

--- scr/release.sh
+++ scr/release.sh
@@ -6,14 +6,14 @@ npm run test &&
 # Get the name of the current branch
 BRANCH=`git rev-parse --symbolic-full-name --abbrev-ref HEAD` &&
 
+# Sort package.json, keeps neat
+npx sort-package-json &&
+
+# Bump the version & changelogs
+npx standard-version &&
+
 # Publish the current branch origin
 if [ "$BRANCH" = "master" ]; then
-    # Sort package.json, keeps neat
-    npx sort-package-json &&
-
-    # Bump the version & changelogs
-    npx standard-version &&
-
     git push origin $BRANCH --follow-tags
 else
     git push origin $BRANCH
