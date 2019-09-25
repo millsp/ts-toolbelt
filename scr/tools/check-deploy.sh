04f3df8a4e5e9e66815e92d6e834a0d7bf69186f
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-09-26 00:57:09 +0200

    fix: delay on CI deploy

--- scr/tools/check-deploy.sh
+++ scr/tools/check-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sleep 30s # we just wait a little for it to propagate
+sleep 60s # we just wait a little for it to propagate
 
 # we replace the travis branch name with "latest" if "master"
 PKG_VERSTAG=$(echo ${TRAVIS_BRANCH//master/latest})
@@ -10,6 +10,7 @@ PKG_VERSION=$(node -p "require('./package.json').version")
 PUB_VERSION=$(npm info .@${PKG_VERSTAG} version)
 
 if [ $PKG_VERSION != $PUB_VERSION ]; then
+    echo 
     exit 1 # travis has just failed the build, force to fail
 fi
 
