d250d43be91d33594ccb9d173ac549133a2704f7
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-09-26 13:50:36 +0200

    fix: infinite recursion on `any`

--- tst/Tuple.ts
+++ tst/Tuple.ts
@@ -191,6 +191,8 @@ type T_FLATTEN = [1, 12, [2, [3, [4, [5, [6, [7, [8, [9, 92]]]]]]]]]
 type FLATTEN_T = [1, 12, 2, 3, 4, 5, 6, 7, 8, 9, 92];
 
 checks([
+    check<T.Flatten<any>,                   any,                    Test.Pass>(),
+    check<T.Flatten<any[]>,                 any[],                  Test.Pass>(),
     check<T.Flatten<T_FLATTEN>,             FLATTEN_T,              Test.Pass>(),
     check<T.Flatten<[1, 2, 42]>,            [1, 2, 42],             Test.Pass>(),
     check<T.Flatten<readonly [1, 2, 42]>,   readonly [1, 2, 42],    Test.Pass>(),
@@ -571,6 +573,7 @@ checks([
 checks([
     check<T.UnNest<[[1], [2],   [3, [4]]]>,     [1, 2, 3, [4]],     Test.Pass>(),
     check<T.UnNest<number[][] | number[]>,      number[],           Test.Pass>(),
+    check<T.UnNest<any>,                        any[],              Test.Pass>(),
 ])
 
 // ---------------------------------------------------------------------------------------
