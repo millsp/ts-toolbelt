a247e02238f47a4e0e51e910f4a25f9c3b1bbf52
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-09-30 19:58:21 +0200

    fix: simpler P.Omit

--- src/Object/P/Omit.ts
+++ src/Object/P/Omit.ts
@@ -1,29 +1,26 @@
-import {Compute} from '../../Any/Compute'
 import {IterationOf} from '../../Iteration/IterationOf'
 import {Iteration} from '../../Iteration/Iteration'
 import {Pos} from '../../Iteration/Pos'
 import {Next} from '../../Iteration/Next'
-import {Filter} from '../Filter'
 import {Path as PPath} from './_Internal'
 import {Index} from '../../Any/Index'
-import {Type} from '../../Any/Type'
 import {EndOf} from '../../Tuple/EndOf'
 import {Tuple} from '../../Tuple/Tuple'
+import {Select} from '../Select'
 
-type Never = Type<{}, 'never'>
-
-type _Omit<O extends object, Path extends Tuple<Index>, I extends Iteration = IterationOf<'0'>> = {
+type _Omit<O extends object, Path extends Tuple<Index>, I extends Iteration = IterationOf<'0'>> =
+Select<{                                // No `never` fields
   [K in keyof O]:
-    O[K] extends infer Prop               // Needed for the below to be distributive
-    ? K extends Path[Pos<I>]              // If K is part of Path
-      ? Pos<I> extends EndOf<Path>        // & if it's the target
-        ? Never // don't pick             // Update - target
-        : Prop extends object             // If it's an object
-          ? _Omit<Prop, Path, Next<I>>    // Continue diving
-          : Prop // pick it               // Part of path, but not object - x
-      : Prop // pick it                   // Not part of path - x
+    O[K] extends infer Prop             // Needed for the below to be distributive
+    ? K extends Path[Pos<I>]            // If K is part of Path
+      ? Pos<I> extends EndOf<Path>      // & if it's the target
+        ? never // don't pick           // Update - target
+        : Prop extends object           // If it's an object
+          ? _Omit<Prop, Path, Next<I>>  // Continue diving
+          : Prop // pick it             // Part of path, but not object - x
+      : Prop // pick it                 // Not part of path - x
     : never
-} & {} extends infer X ? Filter<X & {}, Never, '<-extends'> : never // No `never` fields
+}, any> & {}
 
 /** Remove out of **`O`** the fields at **`Path`**
  * (⚠️ this type is expensive)
