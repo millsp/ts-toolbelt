9a767964a59a43771c4017e286fb1dffd70370f6
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-10-01 15:05:47 +0200

    fix: releases

--- scr/release.sh
+++ scr/release.sh
@@ -15,12 +15,8 @@ npx sort-package-json &&
 npx standard-version &&
 
 # Publish the current branch origin
-if [ "$BRANCH" = "master" ]; then
-    if [ "$1" = "--no-tags" ]; then             # disable tags
-        git push origin $BRANCH
-    elif [ "$RELEASE" = "true" ]; then
-        git push origin $BRANCH --follow-tags   # only releases
-    fi;
+if [ "$BRANCH" != "master" ] || [ "$1" = "--no-tags" ] || [ "$RELEASE" = "false" ]; then
+    git push origin $BRANCH                 # no a release
 else
-    git push origin $BRANCH                     # on branches
+    git push origin $BRANCH --follow-tags   # only releases
 fi;
