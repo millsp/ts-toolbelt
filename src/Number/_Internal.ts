9ab7524be611d0bdc633afed80e1ae8a1623bf5d
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-10-05 13:43:23 +0200

    perf: improved for numbers

--- src/Number/_Internal.ts
+++ src/Number/_Internal.ts
@@ -1,6 +1,4 @@
 import {IterationMap} from '../Iteration/IterationOf'
-import {Exclude} from '../Union/Exclude'
-import {SelectKeys} from '../Object/SelectKeys'
 import {Format} from '../Iteration/Format'
 
 /** Describes compatible type formats
@@ -10,9 +8,15 @@ import {Format} from '../Iteration/Format'
  */
 export type Formats = 'b' | 'n' | 's'
 
-type KnownIterationMapKeys = Exclude<keyof IterationMap, '__'>
-type PositiveIterationKeys = SelectKeys<IterationMap, [any, any, any, any, '+']>
-type NegativeIterationKeys = SelectKeys<IterationMap, [any, any, any, any, '-']>
+// import {Exclude} from '../Union/Exclude'
+// import {SelectKeys} from '../Object/SelectKeys'
+// type KnownIterationMapKeys = Exclude<keyof IterationMap, '__'>
+// type PositiveIterationKeys = SelectKeys<IterationMap, [any, any, any, any, '+']>
+// type NegativeIterationKeys = SelectKeys<IterationMap, [any, any, any, any, '-']>
+
+type KnownIterationMapKeys = '-40' | '-39' | '-38' | '-37' | '-36' | '-35' | '-34' | '-33' | '-32' | '-31' | '-30' | '-29' | '-28' | '-27' | '-26' | '-25' | '-24' | '-23' | '-22' | '-21' | '-20' | '-19' | '-18' | '-17' | '-16' | '-15' | '-14' | '-13' | '-12' | '-11' | '-10' | '-9' | '-8' | '-7' | '-6' | '-5' | '-4' | '-3' | '-2' | '-1' | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12' | '13' | '14' | '15' | '16' | '17' | '18' | '19' | '20' | '21' | '22' | '23' | '24' | '25' | '26' | '27' | '28' | '29' | '30' | '31' | '32' | '33' | '34' | '35' | '36' | '37' | '38' | '39' | '40'
+type PositiveIterationKeys = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12' | '13' | '14' | '15' | '16' | '17' | '18' | '19' | '20' | '21' | '22' | '23' | '24' | '25' | '26' | '27' | '28' | '29' | '30' | '31' | '32' | '33' | '34' | '35' | '36' | '37' | '38' | '39' | '40'
+type NegativeIterationKeys = '-40' | '-39' | '-38' | '-37' | '-36' | '-35' | '-34' | '-33' | '-32' | '-31' | '-30' | '-29' | '-28' | '-27' | '-26' | '-25' | '-24' | '-23' | '-22' | '-21' | '-20' | '-19' | '-18' | '-17' | '-16' | '-15' | '-14' | '-13' | '-12' | '-11' | '-10' | '-9' | '-8' | '-7' | '-6' | '-5' | '-4' | '-3' | '-2' | '-1'
 
 /** Describes known values of a **number**
  */
