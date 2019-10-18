74f593cef89a1524618fb1f1cf30ac1fdeef8326
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-10-18 11:09:24 +0200

    perf(p.pick): use Pos

--- src/Object/P/Pick.ts
+++ src/Object/P/Pick.ts
@@ -12,7 +12,7 @@ import {Key} from '../../Iteration/Key'
 type _Pick<O, Path extends Tuple<Index>, I extends Iteration = IterationOf<'0'>> =
   O extends object                                // If it's an object
   ? OPick<O, Path[Pos<I>]> extends infer Picked   // Pick the current index
-    ? Key<I> extends LastIndex<Path, 's'>         // If it's the last index
+    ? Pos<I> extends LastIndex<Path>              // If it's the last index
       ? Picked                                    // Return the picked object
       : {                                         // Otherwise, continue diving
           [K in keyof Picked]: _Pick<Picked[K], Path, Next<I>>
