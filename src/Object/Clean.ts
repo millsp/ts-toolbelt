015ef00d050ad19ce6a8df9d3f755ac1183f298b
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-10-25 01:28:28 +0200

    chore(o/t.ensure): rename to Clean

--- src/Object/Clean.ts
+++ src/Object/Clean.ts
@@ -8,16 +8,13 @@ import {Numbers} from '../Number/_Internal'
  * visually (or that could at least be simplified for the end user). This will
  * turn anything that is passed to it into a cleaned up **`object`**.
  *
- * If **`O`** has been mixed up with a **Tuple**, then only its own keys will be
- * kept. However, if it's only an **`Array`** or a mix of it, we restore it. And
- * **`object`**s that were mixed with **`Array`**s cannot be untangled.
  * @param O
  * @returns **`object`**
  * @example
  * ```ts
  * ```
  */
-export type Ensure<O extends object> =
+export type Clean<O extends object> =
     number extends At<O, 'length'>                                                    // if it's an array
     ? [Exclude<keyof O, keyof any[] | Numbers['string']['+' | '0']>] extends [never]  //  if it's not mixed
       ? At<O, number>[]                                                               //    restore it
