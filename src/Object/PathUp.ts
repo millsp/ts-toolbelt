5f7918a56bb91f6bdadda9e117749338a8e58740
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-10-26 18:32:47 +0200

    feat(pathup): better path

---
+++ src/Object/PathUp.ts
@@ -0,0 +1,37 @@
+import {IterationOf} from '../Iteration/IterationOf'
+import {Iteration} from '../Iteration/Iteration'
+import {Next} from '../Iteration/Next'
+import {Pos} from '../Iteration/Pos'
+import {Length} from '../Tuple/Length'
+import {At} from './At'
+import {Cast} from '../Any/Cast'
+import {NonNullable as UNonNullable} from '../Union/NonNullable'
+import {Index} from '../Any/Index'
+import {Tuple} from '../Tuple/Tuple'
+
+type _PathUp<O, Path extends Tuple<Index>, I extends Iteration = IterationOf<'0'>> = {
+    0: At<O & {}, Path[Pos<I>]> extends infer OK
+       ? OK extends unknown
+         ? _PathUp<UNonNullable<OK>, Path, Next<I>>
+         : never
+       : never
+    1: O // Use of `NonNullable` otherwise path cannot be followed #`undefined`
+}[
+    Pos<I> extends Length<Path>
+    ? 1 // Stops before going too deep (last key) & check if it has it
+    : 0 // Continue iterating and go deeper within the object with `At`
+]
+
+/** Get in **`O`** the type of nested properties.
+ * It is able to deal with nested **`Union`**s.
+ * @param O to be inspected
+ * @param Path to be followed
+ * @returns **`any`**
+ * @example
+ * ```ts
+ * ```
+ */
+export type PathUp<O extends object, Path extends Tuple<Index>> =
+    _PathUp<O, Path> extends infer X
+    ? Cast<X, any>
+    : never
---
+++ src/Tuple/PathUp.ts
@@ -0,0 +1,15 @@
+import {PathUp as OPathUp} from '../Object/PathUp'
+import {Tuple} from './Tuple'
+import {Index} from '../Any/Index'
+
+/** Get in **`O`** the type of nested properties.
+ * It is able to deal with nested **`Union`**s.
+ * @param T to be inspected
+ * @param Path to be followed
+ * @returns **`any`**
+ * @example
+ * ```ts
+ * ```
+ */
+export type PathUp<O extends Tuple, Path extends Tuple<Index>> =
+    OPathUp<O, Path>
