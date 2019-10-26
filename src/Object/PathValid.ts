8d3a6ffe0aacc439e4fd2d338d15639b538ceb6f
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-10-26 13:30:19 +0200

    docs(o/t.pathvalid): examples

--- src/Object/PathValid.ts
+++ src/Object/PathValid.ts
@@ -13,14 +13,6 @@ import {Prev} from '../Iteration/Prev'
 import {Index} from '../Any/Index'
 import {Tuple} from '../Tuple/Tuple'
 
-/** Replaces invalid parts of a path with `never`
- * @param O to be inspected
- * @param Path to be validated
- * @returns **`string[]`**
- * @example
- * ```ts
- * ```
- */
 type _PathValid<O, Path extends Tuple<Index>, I extends Iteration = IterationOf<'0'>> = {
     0: _PathValid<UNonNullable<At<O & {}, Path[Pos<I>]>>, Path, Next<I>>
     1: Update<Path, KeySet<Key<Prev<I>>, Length<Path, 's'>>, never>
@@ -30,12 +22,31 @@ type _PathValid<O, Path extends Tuple<Index>, I extends Iteration = IterationOf<
     : 0
 ]
 
-/** Get in **`O`** the type of nested properties
+/** Replaces invalid parts of a path with `never`
  * @param O to be inspected
- * @param Path to be followed
- * @returns **`any`**
+ * @param Path to be validated
+ * @returns **`string[]`**
  * @example
  * ```ts
+ * import {A, T, O} from 'ts-toolbelt'
+ *
+ * // Get a property in an object `o` at any depth with `path`
+ * // `A.Cast<P, O.PathValid<O, P>>` makes sure `path` is valid
+ * const getAt = <
+ * O extends object,
+ * P extends T.Tuple<A.Index>
+ * >(o: O, path: A.Cast<P, O.PathValid<O, P>>): O.Path<O, P> => {
+ *     let valueAt = o
+ *
+ *     for (const p of path)
+ *         valueAt = valueAt[p]
+ *
+ *     return valueAt as any
+ * }
+ *
+ * const test0 = getAt({a: {b: {c: 1}}},          ['a', 'b'] as const) // {c: number}
+ * const test1 = getAt({a: {b: {c: 1}}} as const, ['a', 'b'] as const) // {c: 1}
+ * const test2 = getAt({a: {b: {c: 1}}},          ['x'] as const)      // error
  * ```
  */
 export type PathValid<O extends object, Path extends Tuple<Index>> =
--- src/Tuple/PathValid.ts
+++ src/Tuple/PathValid.ts
@@ -9,6 +9,24 @@ import {Tuple} from './Tuple'
  * @returns **`string[]`**
  * @example
  * ```ts
+ * import {A, T, O} from 'ts-toolbelt'
+ *
+ * // Get a property in an array `t` at any depth with `path`
+ * // `A.Cast<P, T.PathValid<T, P>>` makes sure `path` is valid
+ * const getAt = <
+ * T extends T.Tuple,
+ * P extends T.Tuple<A.Index>
+ * >(t: T, path: A.Cast<P, T.PathValid<T, P>>): T.Path<T, P> => {
+ *     let valueAt = t
+ *
+ *     for (const p of path)
+ *         valueAt = valueAt[p]
+ *
+ *     return valueAt as any
+ * }
+ *
+ * const test0 = getAt([[[1]]] as const, ['0', '0'] as const) // [1]
+ * const test1 = getAt([[[1]]] as const, ['1'] as const)      // error, never
  * ```
  */
 export type PathValid<T extends Tuple, Path extends Tuple<Index>> =
