cd690f7a0270f7fc5632091aa6b1a8c320294fa3
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-12-02 19:07:56 +0200

    fix(l.objectof): improved type preservation

---
+++ src/List/ObjectOf.ts
@@ -0,0 +1,23 @@
+import {Omit as OOmit} from '../Object/Omit'
+import {HasAll} from '../Union/_api'
+import {True} from '../Boolean/Boolean'
+import {Cast} from '../Any/Cast'
+
+export type _ObjectOf<T extends object> =
+    HasAll<keyof T, keyof any[]> extends True   // check that is is an array
+    ? number extends (Cast<T, any[]>)['length'] // ^^^ handles mixed up objs
+      ? OOmit<T, Exclude<keyof any[], number>>  // preserves arrays
+      : OOmit<T, keyof any[]>                   // transforms tuples
+    : T
+
+/** Transform a [[Tuple]] or an `Array` into an **`object`**
+ * @param T to transform
+ * @returns **`object`**
+ * @example
+ * ```ts
+ * ```
+ */
+export type ObjectOf<T extends object> =
+    T extends unknown
+    ? _ObjectOf<T>
+    : never
