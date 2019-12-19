b00ec1cfe5423ec49751b6c7cd7dcaad4c0faca6
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2019-12-19 03:45:21 +0200

    fix(l.at): missing strict option

--- src/List/At.ts
+++ src/List/At.ts
@@ -1,12 +1,14 @@
 import {At as OAt} from '../Object/At'
 import {Index} from '../Any/Index'
 import {List} from './List'
+import {Boolean, True} from '../Boolean/Boolean'
 
 /** Get in **`T`** the type of an entry of key **`K`**
  * @param T to extract from
- * @param K to extract at
+ * @param K **`keyof`** to extract at
+ * @param strict (?=`True`) `False` enables using keys `string | number | symbol`
  * @returns **`any`**
  * @example
  */
-export type At<T extends List, K extends Index> =
-    OAt<T, K>
+export type At<T extends List, K extends Index, strict extends Boolean = True> =
+    OAt<T, K, strict>
