f19fed09570b38734ab6e4c6c8a364a1f259a7a3
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2020-02-13 16:48:27 +0200

    feat(assign): deep option

--- src/Object/Assign.ts
+++ src/Object/Assign.ts
@@ -1,26 +1,28 @@
 import {Iteration} from '../Iteration/Iteration'
 import {IterationOf} from '../Iteration/IterationOf'
-import {MergeFlat} from './Merge'
+import {Merge} from './Merge'
 import {Pos} from '../Iteration/Pos'
 import {Next} from '../Iteration/Next'
 import {Length} from '../List/Length'
 import {Cast} from '../Any/Cast'
 import {List} from '../List/List'
 import {Extends} from '../Any/Extends'
+import {Depth} from './_Internal'
 
 /**
 @hidden
 */
-type __Assign<O extends object, Os extends List<object>, I extends Iteration = IterationOf<'0'>> = {
-    0: __Assign<MergeFlat<Os[Pos<I>], O>, Os, Next<I>>
+type __Assign<O extends object, Os extends List<object>, depth extends Depth, I extends Iteration = IterationOf<'0'>> = {
+    0: __Assign<Merge<Os[Pos<I>], O, depth>, Os, depth, Next<I>>
     1: O
 }[Extends<Pos<I>, Length<Os>>]
 
+
 /**
 @hidden
 */
-export type _Assign<O extends object, Os extends List<object>> =
-    __Assign<O, Os> extends infer X
+export type _Assign<O extends object, Os extends List<object>, depth extends Depth> =
+    __Assign<O, Os, depth> extends infer X
     ? Cast<X, object>
     : never
 
@@ -28,14 +30,15 @@ export type _Assign<O extends object, Os extends List<object>> =
 Assign a list of [[Object]] into **`O`** with [[Merge]] (last-in overrides)
 @param O to assign to
 @param Os to assign
+@param depth (?=`'flat'`) to do it deeply
 @returns [[Object]]
 @example
 ```ts
 ```
 */
-export type Assign<O extends object, Os extends List<object>> =
+export type Assign<O extends object, Os extends List<object>, depth extends Depth = 'flat'> =
     O extends unknown
     ? Os extends unknown
-      ? _Assign<O, Os>
+      ? _Assign<O, Os, depth>
       : never
     : never
--- src/Object/AssignUp.ts
+++ src/Object/AssignUp.ts
@@ -7,20 +7,21 @@ import {Length} from '../List/Length'
 import {Cast} from '../Any/Cast'
 import {List} from '../List/List'
 import {Extends} from '../Any/Extends'
+import {Depth} from './_Internal'
 
 /**
 @hidden
 */
-type __AssignUp<O extends object, Os extends List<object>, I extends Iteration = IterationOf<'0'>> = {
-    0: __AssignUp<MergeUp<Os[Pos<I>], O>, Os, Next<I>>
+type __AssignUp<O extends object, Os extends List<object>, depth extends Depth, I extends Iteration = IterationOf<'0'>> = {
+    0: __AssignUp<MergeUp<Os[Pos<I>], O, depth>, Os, depth, Next<I>>
     1: O
 }[Extends<Pos<I>, Length<Os>>]
 
 /**
 @hidden
 */
-export type _AssignUp<O extends object, Os extends List<object>> =
-    __AssignUp<O, Os> extends infer X
+export type _AssignUp<O extends object, Os extends List<object>, depth extends Depth> =
+    __AssignUp<O, Os, depth> extends infer X
     ? Cast<X, object>
     : never
 
@@ -28,14 +29,15 @@ export type _AssignUp<O extends object, Os extends List<object>> =
 Assign a list of [[Object]] into **`O`** with [[MergeUp]] (last-in combines or overrides)
 @param O to assign to
 @param Os to assign
+@param depth (?=`'flat'`) to do it deeply
 @returns [[Object]]
 @example
 ```ts
 ```
 */
-export type AssignUp<O extends object, Os extends List<object>> =
+export type AssignUp<O extends object, Os extends List<object>, depth extends Depth = 'flat'> =
     O extends unknown
     ? Os extends unknown
-      ? _AssignUp<O, Os>
+      ? _AssignUp<O, Os, depth>
       : never
     : never
