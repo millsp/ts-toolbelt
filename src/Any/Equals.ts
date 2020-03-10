91aef20a67da686fda397051f467b00a43db6591
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2020-03-10 13:24:13 +0200

    fix(equals): fix broken null unions #97

--- src/Any/Equals.ts
+++ src/Any/Equals.ts
@@ -13,8 +13,10 @@
  * ```
  */
 export type Equals<A1 extends any, A2 extends any> =
-    (<A>() => A extends A1 ? 1 : 0) extends (<A>() => A extends A2 ? 1 : 0)
-    ? 1
+    (<A>() => A extends A2 ? 1 : 0) extends (<A>() => A extends A1 ? 1 : 0)
+    ? (<A>() => A extends A1 ? 1 : 0) extends (<A>() => A extends A2 ? 1 : 0)
+      ? 1
+      : 0
     : 0
 
 // Credit https://stackoverflow.com/a/52473108/3570903
