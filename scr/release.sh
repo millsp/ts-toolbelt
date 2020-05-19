7016004431c19bffd2900acb8832b0433e475ca5
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2020-05-19 12:43:39 +0200

    ci(release): do not release for bug fixes

--- scr/release.sh
+++ scr/release.sh
@@ -12,8 +12,11 @@ npx sort-package-json &&
 # Bump the version & changelogs
 npx standard-version &&
 
+# check if we have to do a release
+RELEASE=$(node -p "require('./package.json').version.split('.')[2] === '0'") &&
+
 # Publish the current branch origin
-if [ "$BRANCH" != "master" ] || [ "$1" = "--no-tags" ]; then
+if [ "$BRANCH" != "master" ] || [ "$1" = "--no-tags" ] || [ "$RELEASE" = "false" ]; then
     git push origin $BRANCH                 # not a release
 else
     git push origin $BRANCH --follow-tags   # only releases
