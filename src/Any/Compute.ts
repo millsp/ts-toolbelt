6416ac31de6726c92af2e967ebb613cc991ecf4e
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2020-07-09 15:53:00 +0200

    fix(compute): can compute circular refs

--- src/Any/Compute.ts
+++ src/Any/Compute.ts
@@ -1,4 +1,16 @@
 /**
+ * @hidden
+ */
+type _Compute<A extends any, Seen extends any = A> =
+    A extends Function
+    ? A
+    : {
+        [K in keyof A]: A[K] extends Seen               // `Seen` handles circular type refs
+                        ? A[K]                          // we've seen this type, don't compute
+                        : _Compute<A[K], A[K] | Seen>   // 1st time seeing this, save & compute
+      } & {}
+
+/**
  * Force TS to load a type that has not been computed (to resolve composed
  * types that TS haven't fully resolved, for display purposes mostly).
  * @param A to compute
@@ -11,6 +23,4 @@
  * ```
  */
 export type Compute<A extends any> =
-    A extends Function
-    ? A
-    : {[K in keyof A]: A[K]} & {}
+    _Compute<A>
