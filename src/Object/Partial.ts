f1d6fc876a3ca62580eadd27be8d69c533b8794f
Luca Ban <mesqueeb@gmail.com>
2020-07-11 08:33:28 +0900

    fix(partial): format of JSDoc example

--- src/Object/Partial.ts
+++ src/Object/Partial.ts
@@ -6,7 +6,12 @@ Make a type's fields optional. Choose to do it deeply or not.
 @param O the type to make its fields optional
 @param depth (?=`'flat'`) to do it deeply
 @returns [[Object]]
-@example Partial<MyType>
-@example Partial<MyType, 'deep'>
+@example
+```ts
+import { O } from 'ts-toolbelt'
+
+O.Partial<MyType>
+O.Partial<MyType, 'deep'>
+```
  */
 export type Partial<O extends object, depth extends Depth = 'flat'> = OptionalPart<O, depth>
