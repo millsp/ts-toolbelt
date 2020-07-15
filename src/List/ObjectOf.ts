8131b976c5b24249103a7350d35e28e7056b172a
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2020-07-15 23:05:04 +0200

    fix(object): revert beacause of breakage on deps

--- src/List/ObjectOf.ts
+++ src/List/ObjectOf.ts
@@ -2,14 +2,17 @@ import {_Omit} from '../Object/Omit'
 import {At} from '../Object/At'
 import {List} from './List'
 import {Exclude} from '../Union/Exclude'
+import {Has} from '../Union/Has'
 
 /**
 @hidden
 */
-export type _ObjectOf<L extends List> =
-    number extends At<L, 'length'>           // ^^^ handles mixed up objs
-    ? _Omit<L, Exclude<keyof any[], number>> // preserves arrays
-    : _Omit<L, keyof any[]>                  // transforms tuples
+export type _ObjectOf<L extends object> =
+    Has<keyof L, keyof List> extends 1         // check that is is an array
+    ? number extends At<L, 'length'>           // ^^^ handles mixed up objs
+      ? _Omit<L, Exclude<keyof any[], number>> // preserves arrays
+      : _Omit<L, keyof any[]>                  // transforms tuples
+    : L
 
 /**
 Transform a [[List]] into an [[Object]] equivalent
@@ -19,7 +22,7 @@ Transform a [[List]] into an [[Object]] equivalent
 ```ts
 ```
 */
-export type ObjectOf<L extends List> =
+export type ObjectOf<L extends object> =
     L extends unknown
     ? _ObjectOf<L>
     : never
