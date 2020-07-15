faf2b059feaedb2ccab3942bed5c87bdfa6ab3e1
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2020-07-15 23:12:10 +0200

    chore: revert and comment

--- src/List/ObjectOf.ts
+++ src/List/ObjectOf.ts
@@ -1,18 +1,18 @@
 import {_Omit} from '../Object/Omit'
+import {Has} from '../Union/Has'
 import {At} from '../Object/At'
 import {List} from './List'
 import {Exclude} from '../Union/Exclude'
-import {Has} from '../Union/Has'
 
 /**
 @hidden
 */
 export type _ObjectOf<L extends object> =
-    Has<keyof L, keyof List> extends 1         // check that is is an array
+    Has<keyof L, keyof List> extends 1         // check that is is an array // todo rm
     ? number extends At<L, 'length'>           // ^^^ handles mixed up objs
       ? _Omit<L, Exclude<keyof any[], number>> // preserves arrays
       : _Omit<L, keyof any[]>                  // transforms tuples
-    : L
+    : L                                        // todo rm
 
 /**
 Transform a [[List]] into an [[Object]] equivalent
@@ -26,3 +26,5 @@ export type ObjectOf<L extends object> =
     L extends unknown
     ? _ObjectOf<L>
     : never
+
+// todo simplify on the v7, must take Lists only
