ff8f99a144b0edfc00d050aff772ea6e9ca87390
Benoit Ruiz <benoit.ruiz01@gmail.com>
2020-09-25 17:10:02 +0200

    docs: add examples to C.Class, C.Parameters

--- src/Class/Parameters.ts
+++ src/Class/Parameters.ts
@@ -1,12 +1,17 @@
 import {Class} from './Class'
 
 /**
-Get the parameters of a class constructor
-@param C **typeof** class
-@returns [[List]]
-@example
-```ts
-```
+ * Get the parameters of a class constructor
+ * @param C **typeof** class
+ * @returns [[List]]
+ * @example
+ * ```ts
+ * import {C} from 'ts-toolbelt'
+ *
+ * type User = C.Class<[string, string], {firstname: string, lastname: string}>
+ *
+ * type test0 = C.Parameters<User> // [string, string]
+ * ```
 */
 export type Parameters<C extends Class> =
     C extends Class<infer P, any>
