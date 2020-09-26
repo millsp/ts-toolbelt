da05b6349bda5bd55d6e674fd9bb4199e8bb2fd4
Benoit Ruiz <benoit.ruiz01@gmail.com>
2020-09-26 10:14:06 +0200

    docs(list): add examples to L.Compulsory, L.CompulsoryKeys, L.Concat

--- src/List/Compulsory.ts
+++ src/List/Compulsory.ts
@@ -1,17 +1,20 @@
 import {Depth} from '../Object/_Internal'
 import {CompulsoryPart} from '../Object/Compulsory'
 import {List} from './List'
-import {_Pick} from '../Object/Pick'
 
 /**
-Make that `L`'s fields cannot be [[Nullable]] or [[Optional]] (it's like
-[[Required]] & [[NonNullable]] at once).
-@param L to make compulsory
-@param depth (?=`'flat'`) to do it deeply
-@returns [[List]]
-@example
-```ts
-```
-*/
+ * Make that `L`'s fields cannot be [[Nullable]] or [[Optional]] (it's like
+ * [[Required]] & [[NonNullable]] at once).
+ * @param L to make compulsory
+ * @param depth (?=`'flat'`) to do it deeply
+ * @returns [[List]]
+ * @example
+ * ```ts
+ * import {L} from 'ts-toolbelt'
+ *
+ * type test0 = L.Compulsory<[1, 2, 3?, 4?]> // [1, 2, 3, 4]
+ * type test1 = L.Compulsory<['a', 'b' | undefined, 'c', 'd', 'e' | null]> // ['a', 'b', 'c', 'd', 'e']
+ * ```
+ */
 export type Compulsory<L extends List, depth extends Depth = 'flat'> =
     CompulsoryPart<L, depth>
--- src/List/CompulsoryKeys.ts
+++ src/List/CompulsoryKeys.ts
@@ -3,14 +3,17 @@ import {ObjectOf} from './ObjectOf'
 import {List} from './List'
 
 /**
-Get the keys of `L` that are [[Compulsory]]
-
-(⚠️ needs `--strictNullChecks` enabled)
-@param L
-@returns [[Key]]
-@example
-```ts
-```
-*/
+ * Get the keys of `L` that are [[Compulsory]].
+ *
+ * (⚠️ needs `--strictNullChecks` enabled)
+ * @param L
+ * @returns [[Key]]
+ * @example
+ * ```ts
+ * import {L} from 'ts-toolbelt'
+ *
+ * type test0 = L.CompulsoryKeys<[1, 2, 3]> // {0: 1, 1: 2, 2: 3}
+ * ```
+ */
 export type CompulsoryKeys<L extends List> =
     OCompulsory<ObjectOf<L>>
