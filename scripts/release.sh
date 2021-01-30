c1669b41975ad2402dcbacf9d6908ca6c667ca28
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2021-01-30 02:18:25 +0100

    fix(changelog): restore previous behavior and erase logs

--- scripts/release.sh
+++ scripts/release.sh
@@ -14,16 +14,14 @@ RELEASE=$(node -p "require('./package.json').version.split('.')[2] === '0'") &&
 
 # Publish the current branch origin
 if [ "$BRANCH" != "master" ]; then
-    npx standard-version --skip.changelog --skip.tag && # skip changelog
-    git push origin $BRANCH                             # not a release
+    npx standard-version --skip.tag &&        # skip changelog
+    git push origin $BRANCH                   # not a release
 else
     if [ "$RELEASE" = "false" ]; then
-        npx standard-version --skip.tag &&              # gen changelog
+        npx standard-version --skip.tag &&    # gen changelog
         git push origin $BRANCH
     else
-        npx standard-version &&                         # gen changelog
-        git push origin $BRANCH --follow-tags           # only releases
+        npx standard-version &&               # gen changelog
+        git push origin $BRANCH --follow-tags # only releases
     fi;
 fi;
-
-# https://github.com/conventional-changelog/conventional-changelog/tree/master/packages/conventional-changelog-cli
