674ba6e0a9942655bb3857567eb913d21a3f2a72
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2021-01-30 03:27:24 +0100

    chore: cleanup

--- scripts/build/types.sh
+++ scripts/build/types.sh
@@ -4,9 +4,3 @@ mkdir -p out
 
 npx tsc sources/ts-toolbelt.ts -d --emitDeclarationOnly --module amd --outFile out/index.d.ts
 
-# echo "
-# declare module 'ts-toolbelt' {
-#     import main = require('ts-toolbelt');
-#     export = main;
-# }
-# " >> out/index.d.ts
