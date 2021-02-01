309c3b005712a3f1552ea21e59be335dbc4125b1
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2021-02-01 22:11:33 +0100

    perf(a.compute): prevent infinite recursion

--- sources/Any/Compute.ts
+++ sources/Any/Compute.ts
@@ -1,5 +1,8 @@
 import {Depth} from '../Object/_Internal'
 import {BuiltIn} from '../Misc/BuiltIn'
+import {Has} from '../Union/Has'
+import {If} from './If'
+import {Key} from './Key'
 
 /**
  * @hidden
@@ -13,33 +16,32 @@ export type ComputeRaw<A extends any> =
  * @hidden
  */
 type ComputeFlat<A extends any> =
-    A extends BuiltIn
-    ? A
-    : A extends Array<any>
-      ? A extends Array<Record<string | number | symbol, any>>
-        ? Array<{[K in keyof A[number]]: A[number][K]} & unknown>
+    A extends BuiltIn ? A :
+    A extends Array<any>
+    ? A extends Array<Record<Key, any>>
+      ? Array<{[K in keyof A[number]]: A[number][K]} & unknown>
+      : A
+    : A extends ReadonlyArray<any>
+      ? A extends ReadonlyArray<Record<Key, any>>
+        ? ReadonlyArray<{[K in keyof A[number]]: A[number][K]} & unknown>
         : A
-      : A extends ReadonlyArray<any>
-        ? A extends ReadonlyArray<Record<string | number | symbol, any>>
-          ? ReadonlyArray<{[K in keyof A[number]]: A[number][K]} & unknown>
-          : A
-        : {[K in keyof A]: A[K]} & unknown;
+      : {[K in keyof A]: A[K]} & unknown;
 
 /**
  * @hidden
  */
-type ComputeDeep<A extends any> =
-    A extends BuiltIn
-    ? A
-    : A extends Array<any>
-      ? A extends Array<Record<string | number | symbol, any>>
-        ? Array<{[K in keyof A[number]]: ComputeDeep<A[number][K]>} & unknown>
+type ComputeDeep<A extends any, Seen = never> =
+    A extends BuiltIn ? A : If<Has<Seen, A>, A, (
+      A extends Array<any>
+      ? A extends Array<Record<Key, any>>
+        ? Array<{[K in keyof A[number]]: ComputeDeep<A[number][K], A | Seen>} & unknown>
         : A
-    : A extends ReadonlyArray<any>
-      ? A extends ReadonlyArray<Record<string | number | symbol, any>>
-        ? ReadonlyArray<{[K in keyof A[number]]: ComputeDeep<A[number][K]>} & unknown>
-        : A
-      : {[K in keyof A]: ComputeDeep<A[K]>} & unknown;
+      : A extends ReadonlyArray<any>
+        ? A extends ReadonlyArray<Record<Key, any>>
+          ? ReadonlyArray<{[K in keyof A[number]]: ComputeDeep<A[number][K], A | Seen>} & unknown>
+          : A
+        : {[K in keyof A]: ComputeDeep<A[K], A | Seen>} & unknown
+    )>;
 
 /**
  * Force TS to load a type that has not been computed (to resolve composed
