9d0ff6441518c2c9a01cea7726c08acd19eb37d9
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2021-03-06 16:50:44 +0100

    chore(update): remove dirty code

--- sources/Object/Update.ts
+++ sources/Object/Update.ts
@@ -1,40 +1,6 @@
 import {Key} from '../Any/Key'
 import {x} from '../Any/x'
 import {Replace} from '../Union/Replace'
-import {Depth} from './_Internal'
-
-/**
- * @hidden
- */
-type UpdateFlat<O extends object, K extends Key, A extends any> = {
-    [P in keyof O]: P extends K
-                    ? Replace<A, x, O[P]>
-                    : O[P]
-} & {}
-
-/**
- * @hidden
- */
-type __UpdateDeep<O, A extends any> = {
-    [K in keyof O]: Replace<A, x, O[K]> extends infer X
-                    ? X extends object
-                      ? __UpdateDeep<X, A>
-                      : X
-                    : never
-}
-
-/**
- * @hidden
- */
-type _UpdateDeep<O extends object, K extends Key, A extends any, OU = Update<O, K, x | A>> = {
-    [K in keyof OU]: __UpdateDeep<OU[K], A>
-} & {}
-
-/**
- * @hidden
- */
-export type UpdateDeep<O extends object, K extends Key, A extends any> =
-    _UpdateDeep<O, K, A>
 
 /**
  * Update in `O` the fields of key `K` with `A`.
@@ -67,7 +33,8 @@ export type UpdateDeep<O extends object, K extends Key, A extends any> =
  * // }
  * ```
  */
-export type Update<O extends object, K extends Key, A extends any, depth extends Depth = 'flat'> = {
-    'flat': UpdateFlat<O, K, A>
-    'deep': UpdateDeep<O, K, A>
-}[depth]
+export type Update<O extends object, K extends Key, A extends any> = {
+    [P in keyof O]: P extends K
+                    ? Replace<A, x, O[P]>
+                    : O[P]
+} & {}
