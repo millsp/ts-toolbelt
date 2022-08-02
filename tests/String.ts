cb3807554c7d9d5dd4ba7eb2178e6fa19d7b7cbf
世新 <975795875@qq.com>
2022-08-02 21:02:18 +0800

    feat: add enum value to union and some utils

--- tests/String.ts
+++ tests/String.ts
@@ -1,48 +1,60 @@
-import {Test, S} from '../sources'
+import { Test, S } from "../sources";
 
-const {checks, check} = Test
+const { checks, check } = Test;
 
 // ///////////////////////////////////////////////////////////////////////////////////////
 // STRING ////////////////////////////////////////////////////////////////////////////////
 
-type S =
-    | 'hola'
-    | 'ciao!'
+type S = "hola" | "ciao!";
 
 // ---------------------------------------------------------------------------------------
 // AT
 
 checks([
-    check<S.At<S, 1 | 2>, 'o' | 'l' | 'i' | 'a', Test.Pass>(),
-    check<S.At<S, 20>, undefined, Test.Pass>(),
-])
+  check<S.At<S, 1 | 2>, "o" | "l" | "i" | "a", Test.Pass>(),
+  check<S.At<S, 20>, undefined, Test.Pass>(),
+]);
 
 // ---------------------------------------------------------------------------------------
 // JOIN
 
-checks([
-    check<S.Join<S.Split<S, ''>>, S, Test.Pass>(),
-])
+checks([check<S.Join<S.Split<S, "">>, S, Test.Pass>()]);
 
 // ---------------------------------------------------------------------------------------
 // LENGTH
 
-checks([
-    check<S.Length<S>, 4 | 5, Test.Pass>(),
-])
+checks([check<S.Length<S>, 4 | 5, Test.Pass>()]);
 
 // ---------------------------------------------------------------------------------------
 // REPLACE
 
 checks([
-    check<S.Replace<'$1 is nice. $1 is good.', '$1', 'Jane'>, 'Jane is nice. Jane is good.', Test.Pass>(),
-])
+  check<
+    S.Replace<"$1 is nice. $1 is good.", "$1", "Jane">,
+    "Jane is nice. Jane is good.",
+    Test.Pass
+  >(),
+]);
 
 // ---------------------------------------------------------------------------------------
 // SPLIT
 
 checks([
-    check<S.Split<S>, ['h', 'o', 'l', 'a'] | ['c', 'i', 'a', 'o', '!'], Test.Pass>(),
-    check<S.Split<'a.b.c', '.'>, ['a', 'b', 'c'], Test.Pass>(),
-    check<S.Split<'a.b.c.', '.'>, ['a', 'b', 'c', ''], Test.Pass>(),
-])
+  check<
+    S.Split<S>,
+    ["h", "o", "l", "a"] | ["c", "i", "a", "o", "!"],
+    Test.Pass
+  >(),
+  check<S.Split<"a.b.c", ".">, ["a", "b", "c"], Test.Pass>(),
+  check<S.Split<"a.b.c.", ".">, ["a", "b", "c", ""], Test.Pass>(),
+]);
+
+// ---------------------------------------------------------------------------------------
+// StringNumToNum
+
+checks([
+  check<S.StringNumToNum<"0">, 0, Test.Pass>(),
+  check<S.StringNumToNum<"1">, 1, Test.Pass>(),
+  check<S.StringNumToNum<"63">, 63, Test.Pass>(),
+  check<S.StringNumToNum<"64">, 64, Test.Fail>(),
+]);
